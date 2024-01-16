e7b5d3e78bf06645ab0f52f939bb6900da992c46
jespinoza <jespinoza@silta.cl>
2024-01-16 19:25:42 -0300

    Add slices

--- sqrt.go
+++ sqrt.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"slices"
 )
 
 const st = "the root square is: "
@@ -40,6 +41,36 @@ func arrayPlayground() {
 	}
 }
 
+func slicePlayground() {
+	s := make([]string, 3)
+	fmt.Println("emp: ", s, "len:", len(s))
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+
+	s = append(s, "f", "h")
+
+	fmt.Println(s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println(c)
+
+	sli := c[1:2]
+	fmt.Println(sli)
+
+	if slices.Equal(sli, c) {
+		fmt.Println("equals!")
+	} else {
+		fmt.Println("not equals!")
+	}
+
+	multiD := make([][]string, 3)
+	multiD[1] = []string{"1", "2", "3"}
+
+	fmt.Println(multiD)
+}
+
 func main() {
 	x := 8.0
 	fmt.Println(math.Sqrt(x))
@@ -53,4 +84,5 @@ func main() {
 
 	switchGo(2)
 	arrayPlayground()
+	slicePlayground()
 }
