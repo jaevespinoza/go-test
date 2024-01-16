40a68a37b247e71fc089f772430b95416b50537f
jespinoza <jespinoza@silta.cl>
2024-01-16 18:23:35 -0300

    Add more test

--- sqrt.go
+++ sqrt.go
@@ -30,6 +30,16 @@ func switchGo(i int) {
 	whatAmI(2)
 }
 
+func arrayPlayground() {
+	a := [6]int{1, 2, 3, 4, 5, 6}
+	fmt.Printf("Len is %v\n", len(a))
+	fmt.Printf("At space 5 is %v\n", a[4])
+
+	for i := 0; i < len(a); i++ {
+		fmt.Println(a[i])
+	}
+}
+
 func main() {
 	x := 8.0
 	fmt.Println(math.Sqrt(x))
@@ -42,4 +52,5 @@ func main() {
 	}
 
 	switchGo(2)
+	arrayPlayground()
 }
